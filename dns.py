1af49539345c96318449401ee29f75138489f31c
Heavy <hd2319@gmail.com>
2014-01-05 04:12:18 -0800

    Added query header parsing. Working on matching and setting response. Also working on parsing settings file

--- dns.py
+++ dns.py
@@ -11,7 +11,8 @@ class DNSHandler(SocketServer.BaseRequestHandler):
     Takes in DNS requests, processes them and gives an anwer. If DNS forwarding is enabled, will attempt to forward the request.
     TODO: Implement pars-able settings file
     """
-    def prcoessFlags(self, flags):
+    masterDict = {} #Master dictionary containing DNS records to IPs -> a dictionary of dictionaries for each subdirectory
+    def processFlags(self, flags):
         #give a list of flags that describe what's wanted
         #first bit is request or response
         toRet = []
@@ -45,9 +46,60 @@ class DNSHandler(SocketServer.BaseRequestHandler):
 
 
         respCode = (flags >> 9) & 31
+        if respCode == 1:
+            toRet.append("FORMATERROR")
+        if respCode == 2:
+            toRet.append("SERVERFAILURE")
+        if respCode == 3:
+            toRet.append("NAMEERROR")
+        if respCode == 4:
+            toRet.append("NOTIMPLEMENTED")
+        if respCode == 5:
+            toRet.append("REFUSED")
 
 
+    def getNames(self, data, queryCount):
+        names = []
+        for x in range(0, queryCount):
+        #We should have the beginning of the question recrods
+        #The format for the qName is [len][string of len bytes][len]../[0]
+            tmpName = []
+            tmpLen = struct.unpack("<B", data[0])[0]+1
+            tmpName.append(data[1:tmpLen])
+            lastPos = tmpLen
+            while tmpLen:
+                tmpLen = struct.unpack("<B", data[lastPos])[0]
+                lastPos += 1
+                if tmpLen:
+                    tmpName.append(data[lastPos:lastPos+tmpLen])
+                    lastPos = lastPos+tmpLen
+            names.append(tmpName)
+        return names
 
+
+    def processHeader(self, data):
+            #first two bytes should be an ID field
+            transID = struct.unpack(">H", data[0:2])[0] #short
+            flags   = struct.unpack(">H", data[2:4])[0]
+            qCount  = struct.unpack(">H", data[4:6])[0]
+            aCount  = struct.unpack(">H", data[6:8])[0]
+            nsCount = struct.unpack(">H", data[8:10])[0]
+            resCount= struct.unpack(">H", data[10:12])[0] #additional records section
+
+            return (transID, flags, qCount, aCount, nsCount, resCount)
+
+    def lookupNames(self, nameRecordes):
+        for name in nameRecords:
+            #look through our record list and match names for addresses
+            if name[-1] in self.masterDict:
+                curDict = self.masterDict[name[-1]]
+            for x in range(len(name), 0, -1):
+                if name[x] in curDict and x != 0:
+                    curDict = curDict[name[x]]
+                elif x == 0:
+                    return curDict[name[x]]
+                else:
+                    return None
 
 
     def processQuery(self, data):
@@ -55,15 +107,15 @@ class DNSHandler(SocketServer.BaseRequestHandler):
         Processes a DNS query, if it's valid. Blows up in your face otherwise
         """
         try:
-            #first two bytes should be an ID field
-            transID = struct.unpack(">H", data[0:2]) #short
-            flags   = struct.unpack("<H", data[2:4])
-            qCount  = struct.unpack(">H", data[4:6])
-            aCount  = struct.unpack(">H", data[6:8])
-            nsCount = struct.unpack(">H", data[8:10])
-            resCount= struct.unpack(">H", data[10:12]) #additional records section
+            transID, flags, qCount, aCount, nsCount, resCount = self.processHeader(data)
+            flagList = self.processFlags(flags)
 
-            flagList = self.parseFlags(flags)
+            if "QUERY" in flagList:
+                #this is a query
+                nameRecords = self.getNames(data[12:], qCount)
+
+
+
 
         except Exception, ex:
             print "[-] Error attempting to process query"
@@ -80,6 +132,17 @@ class DNSHandler(SocketServer.BaseRequestHandler):
         socket = self.request[1]
         
         toSend = self.processQuery(data)
+
+def readConfig(filePath):
+    #Open a specified file path and read the DNS configuration
+    try:
+        config = {}
+        with open(filePath, 'r') as inFile:
+            for line in inFile:
+                    if line[0] == '[':
+                        config['sections'].append(line.strip('[]'))
+
+
 
 if __name__ == "__main__":
         HOST, PORT = "0.0.0.0", 53
